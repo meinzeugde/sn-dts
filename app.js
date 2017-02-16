6eb59f8c2465cd63595b69318e19c08f0b9620df
Matteo Esche <matteo.esche@INTERNAL.SOLID-SERVISION.COM>
2017-02-16 22:28:19 +0100

    fixed update set source code

--- app.js
+++ app.js
@@ -59,16 +59,6 @@ function download(host, auth, table) {
     } catch (error) {
         console.error(error);
     }
-
-    // var request = http.get(url, function(response) {
-    //     response.pipe(file);
-    //     file.on('finish', function() {
-    //         file.close(callback); // close() is async, call cb after close completes.
-    //     });
-    // }).on('error', function(err) { // Handle errors
-    //     fs.unlink(dest); // Delete the file async. (But we don't check the result)
-    //     if (callback) callback(err.message);
-    // });
 };
 
 init();
